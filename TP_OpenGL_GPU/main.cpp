389393400ccd8a9ffff66d469f7dcd90eccc4b00
jhormanc <hmasterjo@hotmail.com>
2016-01-08 20:55:15 +0100

    normals + uv

--- TP_OpenGL_GPU/main.cpp
+++ TP_OpenGL_GPU/main.cpp
@@ -42,7 +42,117 @@ void APIENTRY debug(GLenum, // source
 	std::cout << "DEBUG: " << message << std::endl;
 }
 
-bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec2> &textures, vector<glm::vec3> &normals, vector<GLushort> &verticesIndex, vector<GLushort> &texturesIndex, vector<GLushort> &normalsIndex)
+struct Mesh
+{
+	vector<glm::vec4> vertices;
+	vector<glm::vec2> textures;
+	vector<glm::vec3> normals;
+	vector<GLushort> verticesIndex;
+	vector<GLushort> texturesIndex;
+	vector<GLushort> normalsIndex;
+
+	void merge(Mesh* m)
+	{
+		int vertices_size = vertices.size();
+		int textures_size = textures.size();
+		int normals_size = normals.size();
+
+		for (int i = 0; i < m->vertices.size(); i++)
+			vertices.push_back(m->vertices[i]);
+
+		for (int i = 0; i < m->textures.size(); i++)
+			textures.push_back(m->textures[i]);
+
+		for (int i = 0; i < m->normals.size(); i++)
+			normals.push_back(m->normals[i]);
+
+		for (int i = 0; i < m->verticesIndex.size(); i++)
+			verticesIndex.push_back(vertices_size + m->verticesIndex[i]);
+
+		for (int i = 0; i < m->texturesIndex.size(); i++)
+			texturesIndex.push_back(textures_size + m->texturesIndex[i]);
+
+		for (int i = 0; i < m->normalsIndex.size(); i++)
+			normalsIndex.push_back(normals_size + m->normalsIndex[i]);
+	}
+};
+
+void indexData(vector<glm::vec4> &vertices, vector<glm::vec2> &textures, vector<glm::vec3> &normals, vector<GLushort> &verticesIndex, vector<GLushort> &texturesIndex, vector<GLushort> &normalsIndex)
+{
+	std::vector<glm::vec4> new_vertices;
+	std::vector<glm::vec2> new_uvs;
+	std::vector<glm::vec3> new_normals;
+
+	if (vertices.size() > 0)
+	{
+		if (verticesIndex.size() > 0)
+		{
+			for (unsigned int i = 0; i < verticesIndex.size(); i++)
+			{
+				unsigned int vertexIndex = verticesIndex[i];
+				glm::vec4 vertex = vertices[vertexIndex - 1];
+				new_vertices.push_back(vertex);
+			}
+		}
+		else
+		{
+			for (unsigned int i = 0; i < vertices.size(); i++)
+			{
+				glm::vec4 vertex = vertices[i];
+				new_vertices.push_back(vertex);
+			}
+		}
+	}
+
+	if (textures.size() > 0)
+	{
+		if (texturesIndex.size() > 0)
+		{
+			for (unsigned int i = 0; i < texturesIndex.size(); i++)
+			{
+				unsigned int uvIndex = texturesIndex[i];
+				glm::vec2 uv = textures[uvIndex - 1];
+				new_uvs.push_back(uv);
+			}
+		}
+		else
+		{
+			for (unsigned int i = 0; i < textures.size(); i++)
+			{
+				glm::vec2 uv = textures[i];
+				new_uvs.push_back(uv);
+			}
+		}
+	}
+
+	if (normals.size() > 0)
+	{
+		if (normalsIndex.size() > 0)
+		{
+			for (unsigned int i = 0; i < normalsIndex.size(); i++)
+			{
+				unsigned int normalIndex = normalsIndex[i];
+				glm::vec3 normal = normals[normalIndex - 1];
+				new_normals.push_back(normal);
+			}
+		}
+		else
+		{
+			for (unsigned int i = 0; i < normals.size(); i++)
+			{
+				glm::vec3 normal = normals[i];
+				new_normals.push_back(normal);
+			}
+		}
+	}
+
+	vertices = new_vertices;
+	normals = new_normals;
+	textures = new_uvs;
+}
+
+bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec2> &textures, vector<glm::vec3> &normals, vector<GLushort> &verticesIndex, vector<GLushort> &texturesIndex, vector<GLushort> &normalsIndex, 
+	const bool indexing_data, const glm::vec3 pos = glm::vec3(0.f), const glm::vec3 scale = glm::vec3(1.f))
 {
 	FILE *file;
 	fopen_s(&file, filename, "r");
@@ -52,10 +162,6 @@ bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec
 		printf("Impossible to open the file !\n");
 		return false;
 	}
-
-	std::vector<glm::vec4> temp_vertices;
-	std::vector<glm::vec2> temp_uvs;
-	std::vector<glm::vec3> temp_normals;
 
 	while (!feof(file))
 	{
@@ -67,19 +173,22 @@ bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec
 		{
 			glm::vec3 vertex;
 			sscanf_s(line, "v %f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-			temp_vertices.push_back(glm::vec4(vertex.x, vertex.y, vertex.z, 1.f));
+			vertices.push_back(glm::vec4(pos.x + vertex.x * scale.x,
+				pos.y + vertex.y * scale.y, 
+				pos.z + vertex.z * scale.z, 
+				1.f));
 		}
 		else if (strncmp(line, "vt", 2) == 0)
 		{
 			glm::vec2 uv;
 			sscanf_s(line, "vt %f %f\n", &uv.x, &uv.y);
-			temp_uvs.push_back(uv);
+			textures.push_back(uv);
 		}
 		else if (strncmp(line, "vn", 2) == 0)
 		{
 			glm::vec3 normal;
 			sscanf_s(line, "vn %f %f %f\n", &normal.x, &normal.y, &normal.z);
-			temp_normals.push_back(normal);
+			normals.push_back(normal);
 		}
 		else if (strncmp(line, "f ", 2) == 0)
 		{
@@ -127,69 +236,11 @@ bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec
 		}
 	}
 
-	if (temp_vertices.size() > 0)
-	{
-		if (verticesIndex.size() > 0)
-		{
-			for (unsigned int i = 0; i < verticesIndex.size(); i++)
-			{
-				unsigned int vertexIndex = verticesIndex[i];
-				glm::vec4 vertex = temp_vertices[vertexIndex - 1];
-				vertices.push_back(vertex);
-			}
-		}
-		else
-		{
-			for (unsigned int i = 0; i < temp_vertices.size(); i++)
-			{
-				glm::vec4 vertex = temp_vertices[i];
-				vertices.push_back(vertex);
-			}
-		}
-	}
+	// Reindexing data
+	if (indexing_data)
+		indexData(vertices, textures, normals, verticesIndex, texturesIndex, normalsIndex);
 
-	if (temp_uvs.size() > 0)
-	{
-		if (texturesIndex.size() > 0)
-		{
-			for (unsigned int i = 0; i < texturesIndex.size(); i++)
-			{
-				unsigned int uvIndex = texturesIndex[i];
-				glm::vec2 uv = temp_uvs[uvIndex - 1];
-				textures.push_back(uv);
-			}
-		}
-		else
-		{
-			for (unsigned int i = 0; i < temp_uvs.size(); i++)
-			{
-				glm::vec2 uv = temp_uvs[i];
-				textures.push_back(uv);
-			}
-		}
-	}
-
-	if (temp_normals.size() > 0)
-	{
-		if (normalsIndex.size() > 0)
-		{
-			for (unsigned int i = 0; i < normalsIndex.size(); i++)
-			{
-				unsigned int normalIndex = normalsIndex[i];
-				glm::vec3 normal = temp_normals[normalIndex - 1];
-				normals.push_back(normal);
-			}
-		}
-		else
-		{
-			for (unsigned int i = 0; i < temp_normals.size(); i++)
-			{
-				glm::vec3 normal = temp_normals[i];
-				normals.push_back(normal);
-			}
-		}
-	}
-
+	// Manually compute normals if necessary
 	if (normals.size() == 0 && verticesIndex.size() > 0)
 	{
 		normals.resize(vertices.size(), glm::vec3(0.0, 0.0, 0.0));
@@ -210,6 +261,11 @@ bool load_obj(const char* filename, vector<glm::vec4> &vertices, vector<glm::vec
 	return true;
 }
 
+void indexDataMesh(Mesh *m)
+{
+	indexData(m->vertices, m->textures, m->normals, m->verticesIndex, m->texturesIndex, m->normalsIndex);
+}
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -225,7 +281,7 @@ int main(void)
 	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(WIDTH, HEIGHT, "Open Portal", NULL, NULL);
 	if (!window)
 	{
 		std::cerr << "Could not init window" << std::endl;
@@ -369,52 +425,101 @@ struct
 	glm::vec3 camPos;
 	GLfloat near, far, fov;
 	GLuint buffer;
+	GLuint normalsBuffer;
+	GLuint colorbuffer;
 
 	GLfloat size;
 } gs;
 
+Mesh* createMesh(const char* filename, const bool index_data, const glm::vec3 pos = glm::vec3(0.f), const glm::vec3 scale = glm::vec3(1.f))
+{
+	Mesh *m = new Mesh();
+	if (load_obj(filename, m->vertices, m->textures, m->normals, m->verticesIndex, m->texturesIndex, m->normalsIndex, index_data, pos, scale))
+		return m;
+	return nullptr;
+}
+
 void init()
 {
+	glClearColor(0.f, 0.f, 0.f, 1.f);
+	glClearDepth(1.0f); // Set background depth to farthest
+	glEnable(GL_DEPTH_TEST); // Enable depth testing for z-culling
+	glDepthFunc(GL_LEQUAL); // Set the type of depth-test
+	glShadeModel(GL_SMOOTH); // Enable smooth shading
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST); // Nice perspective corrections
+
 	// Build our program and an empty VAO
 	gs.program = buildProgram("basic.vsl", "basic.fsl");
 
-	glCreateVertexArrays(1, &gs.vao);
-	glGenBuffers(1, &gs.buffer);
-
+	// Global parameters
 	gs.start = clock();
-	gs.p = glm::vec4(0.f, 0.f, 0.f, 0.f);
-	gs.camPos = glm::vec3(0.f, 0.f, -5.f);
+	gs.p = glm::vec4(0.f, 3.f, 0.f, 0.f);
+	gs.camPos = glm::vec3(0.f, 5.f, -10.f);
 	gs.near = 0.1f;
 	gs.far = 100.f;
 	gs.fov = 45.f;
 
-	vector<glm::vec4> vertices;
-	vector<glm::vec2> textures;
-	vector<glm::vec3> normals;
-	vector<GLushort> verticesIndex;
-	vector<GLushort> texturesIndex;
-	vector<GLushort> normalsIndex;
-	load_obj("cube.obj", vertices, textures, normals, verticesIndex, texturesIndex, normalsIndex);
+	Mesh *mesh = createMesh("Stormtrooper.obj", false);
 
-	gs.size = vertices.size();
-	for (int i = 0; i < vertices.size(); i++)
+	if (mesh != nullptr)
 	{
-		std::cout << vertices[i].x << " " << vertices[i].y << " " << vertices[i].z << "\n";
+		Mesh *wall_1 = createMesh("Cube.obj", false, glm::vec3(0.f, -10.f, 0.f), glm::vec3(10.f));
+		mesh->merge(wall_1);
 	}
-	//float data[16] = { -0.5f, -0.5f, 0.f, 1.f, 0.5f, -0.5f, 0.f, 1.f, 0.5f, 0.5f, 0.f, 1.f, -0.5f, 0.5f, 0.f, 1.f };
+	
+	if (mesh != nullptr)
+	{
+		indexDataMesh(mesh);
+		gs.size = mesh->vertices.size();
 
-	glBindVertexArray(gs.vao);
+		// Vertex buffer
+		glGenBuffers(1, &gs.buffer);
+		glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
+		glBufferData(GL_ARRAY_BUFFER, mesh->vertices.size() * sizeof(glm::vec4), &mesh->vertices[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
-	glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(glm::vec4), &vertices[0], GL_STATIC_DRAW);
+		// Normals buffer
+		glGenBuffers(1, &gs.normalsBuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, gs.normalsBuffer);
+		glBufferData(GL_ARRAY_BUFFER, mesh->normals.size() * sizeof(glm::vec3), &mesh->normals[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glVertexAttribPointer(11, 4, GL_FLOAT, GL_FALSE, sizeof(glm::vec4), 0);
-	//glVertexAttribPointer(12, 1, GL_FLOAT, GL_FALSE, sizeof(float) * 4, (void *)12);
+		// Textures buffer
+		glGenBuffers(1, &gs.colorbuffer);
+		glBindBuffer(GL_ARRAY_BUFFER, gs.colorbuffer);
+		glBufferData(GL_ARRAY_BUFFER, mesh->textures.size() * sizeof(glm::vec2), &mesh->textures[0], GL_STATIC_DRAW);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-	glEnableVertexArrayAttrib(gs.vao, 11);
-	//glEnableVertexArrayAttrib(gs.vao, 12);
+		glCreateVertexArrays(1, &gs.vao);
+		glBindVertexArray(gs.vao);
 
-	glBindVertexArray(0);
+		// Vertex shader input vec4 pt
+		glBindBuffer(GL_ARRAY_BUFFER, gs.buffer);
+		glEnableVertexArrayAttrib(gs.vao, 11);
+		glVertexAttribPointer(11, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		//  Vertex shader input vec3 normal
+		glBindBuffer(GL_ARRAY_BUFFER, gs.normalsBuffer);
+		glEnableVertexArrayAttrib(gs.vao, 12);
+		glVertexAttribPointer(12, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		//  Vertex shader input vec2 uv
+		glBindBuffer(GL_ARRAY_BUFFER, gs.colorbuffer);
+		glEnableVertexArrayAttrib(gs.vao, 13);
+		glVertexAttribPointer(13, 2, GL_FLOAT, GL_FALSE, 0, (void *)0);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+		glBindVertexArray(0);
+	}
+	else
+	{
+		std::cerr << "program loading mesh error" << std::endl;
+
+		glfwTerminate();
+		exit(-1);
+	}
 }
 
 void render(GLFWwindow* window)
@@ -426,7 +531,7 @@ void render(GLFWwindow* window)
 	glViewport(0, 0, width, height);
 
 	float c = (float)(clock() - gs.start) / CLOCKS_PER_SEC;
-	glProgramUniform1f(gs.program, 3, std::abs(cos(c)));
+	//glProgramUniform1f(gs.program, 3, std::abs(cos(c)));
 
 	glm::vec3 pt(gs.p.x, gs.p.y, gs.p.z);
 	glm::mat4x4 m;
